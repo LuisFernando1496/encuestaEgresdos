3952d7b1b31b6027e19d8e479e53dba08b7ffdae
LuisFernando1496 <luisfernando_orantes@outlook.com>
2022-04-05 17:45:12 -0500

    Traduciendo toda la app

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,11 +12,7 @@
             </div>
         @endif
        
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar</a>
-                 </div>
-            @endif
+            
          
         <form method="POST" action="{{ route('login') }}">
             @csrf
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,6 +16,22 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('Job Bank')">
+                        {{ __('Job Bank') }}
+                    </x-jet-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('Extracurricular Activities')">
+                        {{ __('Extracurricular Activities') }}
+                    </x-jet-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('Quiz')">
+                        {{ __('Quiz') }}
+                    </x-jet-nav-link>
+                </div>
+
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -99,6 +115,8 @@
                             <x-jet-dropdown-link href="{{ route('profile.show') }}">
                                 {{ __('Profile') }}
                             </x-jet-dropdown-link>
+                            
+                           
 
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                                 <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
@@ -139,6 +157,15 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('Job Bank')">
+                {{ __('Job Bank') }}
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('Extracurricular Activities')">
+                {{ __('Extracurricular Activities') }}
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('Quiz')">
+                {{ __('Quiz') }}
             </x-jet-responsive-nav-link>
         </div>
 
